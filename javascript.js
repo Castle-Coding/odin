cd089d2eb6099bf81876fb218c4564712cc03004
Castle <codelikecastle@gmail.com>
2022-11-08 13:18:42 -0600

    Add event listeners and functions to some buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,46 @@
+const inputDisplay = document.getElementById('input-text-box')
+const inputButtons = document.querySelectorAll('.input-button');
+const operationButtons = document.querySelectorAll('.operation-button')
+const clearButton = document.getElementById('clear-button')
+const backSpaceButton = document.getElementById('backspace-button')
+
+
+let operation = "";
+
+inputButtons.forEach(function (button) {
+    button.value = button.textContent;
+    button.addEventListener('click', function() {
+        if (inputDisplay.value.length < 13 && inputDisplay.value != "") {
+        inputDisplay.value += this.value;
+        } else if (inputDisplay.value.length < 13 && inputDisplay.value == "") {
+            inputDisplay.value = this.value;
+        }
+    })
+})
+
+operationButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        operation = this.value;
+        console.log(operation)
+    })
+})
+
+clearButton.addEventListener('click', function() {
+    inputDisplay.value = "";
+    operation = "";
+})
+
+backSpaceButton.addEventListener('click', function() {
+    if (inputDisplay.value != "" && inputDisplay.value.length > 1) {
+        let a = Array.from(inputDisplay.value)
+        a.pop();
+        inputDisplay.value = parseInt(a.join(""));
+    } else {
+        inputDisplay.value = "";
+    }
+})
+
+
 const footer = document.getElementsByClassName('footer');
 footer[0].textContent = `Copyright © ${new Date().getFullYear()} Castle Coding`;
 
@@ -19,14 +62,14 @@ function operate(operator, a, b) {
             return add([a, b]) // code block
             break;
         case "subtraction":
-            return subtract(a,b) // code block
+            return subtract(a, b) // code block
             break;
         case "multiplication":
-            return multiply([a,b])// code block
+            return multiply([a, b])// code block
             break;
         case "divison":
-            if (divide(a,b) != Infinity) {
-            return divide(a,b) 
+            if (divide(a, b) != Infinity) {
+                return divide(a, b)
             } else return "Undefined"
             break;
     }
