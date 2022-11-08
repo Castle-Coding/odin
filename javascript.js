a30d27291165cb08ec12cb710fc675a82bc6fb02
Castle <codelikecastle@gmail.com>
2022-11-08 12:36:45 -0600

    Create operate function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 const footer = document.getElementsByClassName('footer');
 footer[0].textContent = `Copyright © ${new Date().getFullYear()} Castle Coding`;
 
-
 const subtract = (a, b) => a - b;
 
 const add = (array) => array.reduce((total, current) => total + current, 0);
@@ -14,3 +13,21 @@ const multiply = function (array) {
 
 const divide = (a, b) => a / b;
 
+function operate(operator, a, b) {
+    switch (operator) {
+        case "addition":
+            return add([a, b]) // code block
+            break;
+        case "subtraction":
+            return subtract(a,b) // code block
+            break;
+        case "multiplication":
+            return multiply([a,b])// code block
+            break;
+        case "divison":
+            if (divide(a,b) != Infinity) {
+            return divide(a,b) 
+            } else return "Undefined"
+            break;
+    }
+}
